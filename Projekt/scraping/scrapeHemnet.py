89edc7480403178461547d731536926b13043b40
Nathanotal <nathan.allard@gmail.com>
2022-12-22 12:03:45 +0100

    scraping dilemma

--- Projekt/scraping/scrapeHemnet.py
+++ Projekt/scraping/scrapeHemnet.py
@@ -1,116 +1,201 @@
-# For data gathering
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.options import Options
 import csv
-import sys
 import os
-import time
 import requests
-import pandas as pd
-import datetime
 from tqdm import tqdm
 from concurrent import futures
 from bs4 import BeautifulSoup
+import utilityFunctions as uf
+import time
 
 PATH = os.path.dirname(os.path.abspath(__file__))
-CHROMEDRIVERPATH = PATH + '/chromedriver.exe'
-STADSDELAR_PATH = PATH + '/stadsdelar.txt'
-chrome_options = Options()
-chrome_options.add_argument("disable-popup-blocking")
-
-
-class BostadsData:
-    def __init__(self) -> None:
-        pass
-    
+HEADERS = ['link', 'area', 'streetName', 'number', 'sqm', 'rooms', 'Slutpris', 'Utropspris', 'Prisutveckling', 'Såld eller borttagen', 'Slutpris/m²', 'Dagar på Booli', 'Avgift', 'Bostadstyp', 'Driftskostnad', 'Våning', 'Byggår', 'BRF', 'brfLink', 'Energiklass']
+LOAD_DATA = True
+MAX_WORKERS = min(32, (os.cpu_count() or 1) + 4)
 
 def main():
-    LOAD = False
+    print('Initializing...')
     links = None
-    if LOAD:
-        df = pd.read_csv('vasastan.csv')
-        links = df['link'].tolist()
+    if LOAD_DATA:
+        links = uf.loadLinks()
     else:
+        input('Are you sure you want to scrape all data? Press enter to continue...')
         links = getAllLinks()
+        uf.saveLinks(links)
+    
+    getAndWirteData(links)
+    print('Done!')
+                
+def getAndWirteData(links):
+    with open(f'{PATH}/data/apartmentData.csv', 'w', newline='', encoding='utf-8') as f:
+        # Make a writer which can handle nordic characters
+        writer = csv.writer(f, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        writer.writerow(HEADERS)
         
-    # Get the data for each link
-    for link in tqdm(links):
-        pass
-        
-
-def getStadsDelar():
-    with open(STADSDELAR_PATH, 'r') as f:
-        stadsDelar = f.read().splitlines()
-    return stadsDelar
+        # Write the data for each apratment
+        print('Getting data from pages...')
+        for link in tqdm(links): # This can't be done in parallell because of 429
+            data = getAttributesFromPage(link)
+            if data is not None:
+                # uf.checkDifferenceInData(data, HEADERS) # Debug
+                uf.writeToCsv(data, writer, HEADERS)
      
 def getAllLinks():
-    stadsDelar = getStadsDelar()
+    stadsDelar = uf.getStadsDelar()
     allLinks = []
     
+    print('Getting links to apartments...')
     for stadsDel in tqdm(stadsDelar):
-        allLinks.extend(getLinksFromSearchPage(stadsDel))
+        allLinks.extend(getLinksFromSearchStadsDel(stadsDel))
 
-    # Save all links as a CSV file
-    with open('links.csv', 'w') as f:
-        f.write('link')
-        for link in allLinks:
-            f.write(link + '\n')
+    return allLinks
         
-
-def getNoResults(stadsDel):
-    # Return the number of results for a search query
-    response = requests.get(f'https://www.booli.se/slutpriser/{stadsDel}?objectType=L%C3%A4genhet&page=1')
-    soup = BeautifulSoup(response.text, 'html.parser')
-    
-    # Get the span with class "p3oVj"
-    noApts = soup.find('span', class_='p3oVj')
-    
-    # Get the text from the span
-    no = noApts.text
-    
-    # Parse the text to an integer
-    noInt = int(no.replace(' ', ''))
-    
-    return noInt
-       
-def getLinksFromSearchPage(stadsDel):
+def getLinksFromSearchStadsDel(stadsDel):
     # Return a list of links to all pages of the search result
-    noResults = getNoResults(stadsDel)
+    noResults = uf.getNoResults(stadsDel)
     noLoops = noResults // 35 + 1
     houseLinks = []
-    for pageNo in range(noLoops):
-        response = requests.get(f'https://www.booli.se/slutpriser/{stadsDel}?objectType=L%C3%A4genhet&page={pageNo+1}')
+    
+    # This should be done in parrallel
+    with futures.ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
+        for pageNo in range(noLoops):
+            try:
+                link = f'https://www.booli.se/slutpriser/{stadsDel}?objectType=L%C3%A4genhet&page={pageNo+1}'
+                res = executor.submit(getLinksFromSearchPage, link).result()
+                if res is not None:
+                    houseLinks.extend(res)
+                else:
+                    print('Err', res)
+            except:
+                pass                
+    
+    return houseLinks
+
+
+def getLinksFromSearchPage(link):
+    response = requests.get(link)
         
-        if response.status_code != 200:
-            print('Failed to get page: ', pageNo)
-            print('Error: ', response.status_code)
-            print(stadsDel)
-        
+    if response.status_code == 200:
         # Parse the HTML and save all links (href) in anchor tags with class "_3xhWw"
         soup = BeautifulSoup(response.text, 'html.parser')
         
         # Find all links with class "_3xhWw"
         links = soup.find_all('a', class_='_3xhWw')
         
-        
         # Get the href attribute from each link
         hrefs = [link.get('href') for link in links]
         
-        # Add the hrefs to the list of house links
-        houseLinks.extend(hrefs)
-    
-    return houseLinks
-    
-    
-    
-    
+        return hrefs
+    elif response.status_code == 429:
+        # Too many requests, wait a minute and try again
+        print('Too many requests, waiting 60 seconds...')
+        time.sleep(60)
+    else:
+        print('Error: ', response.status_code)
+        print('Failed to get links from search page: ', link)
+        return []
 
+def getHeadInfo(soup):
+    addressAndPriceClass = 'lzFZY _10w08'
+    sqmAndMoreClass = '_1544W _10w08'
+    
+    # Get the first h1 tag with addressAndPriceClass (the price)
+    address = soup.find('h1', class_=addressAndPriceClass).text
+    
+    # Get all the h4 tags with sqmAndMoreClass (the type and area)
+    sqmRoomsArea = soup.find_all('h4', class_=sqmAndMoreClass)
+    
+    sqmAndRooms = ''
+    area = ''
+    for index, h4 in enumerate(sqmRoomsArea):
+        if index == 0:
+            sqmAndRooms = h4.text
+        elif index == 1:
+            area = h4.text.split(',')[1]
+        else:
+            print('Error: More than 2 h4 tags with class sqmAndMoreClass')
+            print(sqmRoomsArea)
+    
+    # Get the second h4 with sqmAndMoreClass (the type and area)
+    sqmRoom = sqmAndRooms.split(',')
+    sqm = uf.getNumberFromText(sqmRoom[0])
+    rooms = uf.getNumberFromText(sqmRoom[1])
+    
+    streetName, number = uf.cleanAddress(address)
+    
+    data = {'sqm': uf.cleanSqm(sqm), 'rooms': rooms, 'area': area.replace(' ', ''), 'streetName': streetName, 'number': number}
+    
+    return data
+    
+def getGeneralInfo(soup):
+    infoDivClass = '_36W0F mz1O4'
+    attributeClass = 'DfWRI _1Pdm1 _2zXIc sVQc-'
+    nameClass = '_2soQI'
+    valueClass = '_18w8g'
+    
+    # Find the div with the info
+    infoDiv = soup.find('div', class_=infoDivClass)
+    
+    # Find all attributes
+    attributes = infoDiv.find_all('div', class_=attributeClass)
+    
+    # Get the name and value for each attribute
+    data = {}
+    for attribute in attributes:
+        name = attribute.find('div', class_=nameClass).text
+        value = attribute.find('div', class_=valueClass).text
+        # Switch on the name
+        match name:
+            case 'BRF':
+                # If the attribute is a BRF, get the link to the page
+                link = attribute.find('a').get('href')
+                data['brfLink'] = link
+                data[name] = value
+            case 'Slutpris' | 'Utropspris':
+                data[name] = uf.cleanPrice(value)
+            case 'Prisutveckling':
+                data[name] = uf.cleanDiff(value)
+            case 'Avgift' | 'Driftskostnad':
+                data[name] = uf.cleanAvgift(value)
+            case 'Slutpris/m²':
+                data[name] = uf.cleanPriceSqm(value)
+            case 'Storlek' | 'Sista bud' | 'Tomtstorlek' | 'Upplåtelseform' | 'Sidvisningar' | 'Dagar som snart till salu' | 'Biarea' | 'Boendekostnad' | 'Kvadratmeterpris':
+                pass
+            case _:
+                data[name] = value
+        
+    
+    return data
 
-
+def getAttributesFromPage(shortLink):
+    # Return a dictionary with the attributes of a house
+    data = {'link': shortLink}
+    link = f'https://www.booli.se{shortLink}'
+    response = requests.get(link)
+    
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        try:
+            data.update(getHeadInfo(soup))
+        except:
+            pass
+        try:
+            data.update(getGeneralInfo(soup))
+        except:
+            pass
+    elif response.status_code == 429:
+        timeToWait = 5
+        try:
+            timeToWait = int(response.headers['Retry-After'])
+        except:
+            pass
+        time.sleep(timeToWait)
+        return getAttributesFromPage(shortLink)
+    else:
+        print('Error: ', response.status_code)
+        
+    return data
+    
 
 if __name__ == "__main__":
     main()
+
---
+++ Projekt/scraping/utilityFunctions.py
@@ -0,0 +1,138 @@
+from bs4 import BeautifulSoup
+import requests
+import csv
+import os
+import re
+
+PATH = os.path.dirname(os.path.abspath(__file__))
+
+def getNoResults(stadsDel):
+    # Return the number of results for a search query
+    response = requests.get(f'https://www.booli.se/slutpriser/{stadsDel}?objectType=L%C3%A4genhet&page=1')
+    soup = BeautifulSoup(response.text, 'html.parser')
+    
+    # Get the span with class "p3oVj"
+    noApts = soup.find('span', class_='p3oVj')
+    
+    # Get the text from the span
+    no = noApts.text
+    
+    # Parse the text to an integer
+    noInt = int(no.replace(' ', ''))
+    
+    return noInt
+
+def cleanAddress(address):
+    # Separate the number and the street name and remove every - and all spaces
+    number = getNumberFromText(address)
+    address = address.replace(number, '')
+    address = address.replace('-', '')
+    address = address.replace(' ', '')
+    return address, number
+
+def cleanPrice(price):
+    # Remove all spaces and the kr
+    price = price.replace(' ', '')
+    price = price.replace('kr', '')
+    return price
+
+def cleanSqm(sqm):
+    # Remove all spaces and the m²
+    sqm = sqm.replace(' ', '')
+    sqm = sqm.replace('m²', '')
+    return sqm
+
+def cleanAvgift(avgift):
+    # Remove all spaces and the kr/mån
+    avgift = avgift.replace(' ', '')
+    avgift = avgift.replace('kr/mån', '')
+    return avgift
+
+def cleanDiff(diff):
+    # Example of a diff: ±0 kr (+/-0 %)
+    # Example of a negative diff: -55 000 kr (-1.4 %)
+    # Example of a positive diff: +55 000 kr (+1.4 %)
+    # Remove all of the spaces, the kr and everything in the parentheses
+    try:
+        diff = diff.split('(')[0]
+    except:
+        pass
+    diff = diff.replace('±', '')
+    diff = diff.replace('kr', '')
+    diff = diff.replace(' ', '')
+    
+    return diff  
+
+def cleanPriceSqm(priceSqm):
+    # Remove all spaces and the kr/m²
+    priceSqm = priceSqm.replace(' ', '')
+    priceSqm = priceSqm.replace('kr/m²', '')
+    return priceSqm
+ 
+def getStadsDelar():
+    with open(f'{PATH}/stadsdelar.txt', 'r') as f:
+        stadsDelar = f.read().splitlines()
+    return stadsDelar
+
+def getNumberFromText(text):
+    # Return a list of numbers from a string
+    numbers = re.findall(r'\d+', text)
+    if len(numbers) == 0:
+        return ''
+    else:
+        return numbers[0]
+
+def saveLinks(allLinks):
+    with open(f'{PATH}/data/links.csv', 'w') as f:
+        f.write('link\n')
+        for link in allLinks:
+            f.write(link + '\n')
+
+def loadLinks():
+    with open(f'{PATH}/data/links.csv', 'r') as f:
+        allLinks = f.read().splitlines()
+    return allLinks[1:]
+
+def writeToCsv(data, writer, headers):
+    row = []
+    for header in headers:
+        value = ''
+        try:
+            value = data[header]
+        except KeyError:
+            pass
+        row.append(value)
+    writer.writerow(row)
+    
+def checkDifferenceInData(data, headers):
+    # If there is an attribute which is not in the dictionary, throw an error and inspect the data
+    diff = set(data.keys()) - set(headers)
+    if len(diff) > 0:
+        print('Error, there is a new attribute: \n')
+        print(diff)
+        
+
+def splitCSVIntoNFiles(fileName, n):
+    # Split a csv file into n files
+    with open(f'{PATH}/data/{fileName}.csv', 'r') as f:
+        reader = csv.reader(f)
+        data = [row for row in reader]
+    
+    # Calculate the number of rows in each file
+    noRows = len(data)
+    noRowsPerFile = noRows // n
+    for i in range(n):
+        # Write the header
+        with open(f'{PATH}/data/split/{fileName}_{i+1}.csv', 'w', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow(data[0])
+        
+        # Write the rows
+        with open(f'{PATH}/data/split/{fileName}_{i+1}.csv', 'a', newline='') as f:
+            writer = csv.writer(f)
+            for j in range(noRowsPerFile):
+                toWrite = data[i * noRowsPerFile + j + 1]
+                writer.writerow(toWrite)
+                
+# splitCSVIntoNFiles('links', 13)
+    
